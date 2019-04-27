e09daa7b30db45a2ed62228af42b0560352eaa69
Csaba Kertesz <csaba.kertesz@gmail.com>
2019-04-27 21:56:02 +0300

    Add optimized CUDA package building

---
+++ 3_build_tensorflow_cpp_optimized_cuda_x86_64.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Get the directory where the script is stored
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#
+# Generate the binaries
+#
+# Clean old packaging files if they exist
+mkdir -p ${DIR}/packaging/libs
+rm -rf ${DIR}/packaging/libs/*
+
+# Clone tensorflow_cc
+cd ${DIR}
+git clone https://github.com/FloopCZ/tensorflow_cc/
+
+# Clean up
+cd ${DIR}/tensorflow_cc/ || exit 1
+git clean -fdx
+git reset --hard
+mkdir -p build && mkdir -p install && cd build || exit 1
+
+export GCC_HOST_COMPILER_PATH=/usr/bin/gcc
+export CC_OPT_FLAGS="-march=skylake"
+cmake -DTENSORFLOW_STATIC=OFF -DTENSORFLOW_SHARED=ON -DTENSORFLOW_TAG=v1.13.1 \
+      -DCMAKE_INSTALL_PREFIX=${DIR}/tensorflow_cc/install -B. -H../tensorflow_cc || exit 1
+make && make install || exit 1
+
+# Copy/prepare the final binaries
+cp ${DIR}/tensorflow_cc/install/lib/tensorflow_cc/libtensorflow_cc.so ${DIR}/packaging/libs/ || exit 1
+cp ${DIR}/tensorflow_cc/install/lib/tensorflow_cc/libprotobuf.a ${DIR}/packaging/libs/ || exit 1
+
+echo READY! Libraries are extracted to ${DIR}/packaging/libs/
--- 4_generate_optimized_cuda_x86_64_package.sh
+++ 4_generate_optimized_cuda_x86_64_package.sh
@@ -8,7 +8,7 @@ if [ ! -d $DIR/packaging/headers ]; then
   exit 1
 fi
 if [ ! -d $DIR/packaging/libs ]; then
-  echo Get the Tensorflow C++ libraries with the 3_build_tensorflow_cpp_generic_cuda_x86_64.sh script
+  echo Get the Tensorflow C++ libraries with the 3_build_tensorflow_cpp_optimized_cuda_x86_64.sh script
   exit 1
 fi
 
@@ -26,11 +26,11 @@ DATE=20190225
 cd $DIR/packaging
 rm -rf debian/.debhelper debian/tensorflow-cpp-*
 rm -rf debian/debhelper-build-stamp debian/files
-cmake . -DGIT_TAG=${TAG} -DGIT_COMMIT=${COMMIT} -DGIT_DATE=${DATE} -DPKG_SUFFIX=generic-cuda -DCMAKE_INSTALL_PREFIX=/usr -DPKG_ARCH=amd64 -DRELEASE_MODE=OFF
+cmake . -DGIT_TAG=${TAG} -DGIT_COMMIT=${COMMIT} -DGIT_DATE=${DATE} -DPKG_SUFFIX=optimized-cuda -DCMAKE_INSTALL_PREFIX=/usr -DPKG_ARCH=amd64 -DRELEASE_MODE=OFF
 dpkg-buildpackage -rfakeroot -b -nc
 
 # Make a plain tar file
-tar -C ./debian/tensorflow-cpp-generic-cuda/ -cf ../tensorflow-cpp-generic-cuda_${TAG}~git${DATE}~${COMMIT}.tar usr/
-pxz -9 ../tensorflow-cpp-generic-cuda_${TAG}~git${DATE}~${COMMIT}.tar
+tar -C ./debian/tensorflow-cpp-optimized-cuda/ -cf ../tensorflow-cpp-optimized-cuda_${TAG}~git${DATE}~${COMMIT}.tar usr/
+pxz -9 ../tensorflow-cpp-optimized-cuda_${TAG}~git${DATE}~${COMMIT}.tar
 
-echo READY! Generic/CUDA x86_64 package is generated!
+echo READY! Optimized/CUDA x86_64 package is generated!
